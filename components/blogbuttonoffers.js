b01473cb838b9e08e914b45ac5692570d1b3b1e1
„PersonalTaxi” <marek.marszalek@onesta.com.pl>
2024-04-12 20:59:03 +0200

    first commit

--- components/blogbuttonoffers.js
+++ components/blogbuttonoffers.js
@@ -7,14 +7,14 @@ export default function Blogbuttonoffers() {
 
   useEffect(() => {
     setTimeout(() => {
-      showPropertiesOffers.current.style.top = "70px";
+      showPropertiesOffers.current.style.top = "80px";
     }, 5000);
   }, []);
 
   return (
     <div ref={showPropertiesOffers} className="fixed right-[10px] top-[10px] duration-200">
       <Link
-        href="/hiszpania"
+        href="/hiszpania?page=1"
         className="text-white md:text-[20px] text-[12px] font-normal flex items-center py-[5px] rounded-b-2xl justify-center bg-red-500 md:w-[330px] md:px-0 px-[5px] shadow-xl mb-[200px] mx-auto z-[999] md:z-auto"
       >
         Oferty nieruchomości
