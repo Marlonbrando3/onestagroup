d0db84bb0971238758367c64466bb622607ffbb2
„PersonalTaxi” <marek.marszalek@onesta.com.pl>
2024-04-16 14:45:50 +0200

    first commit

--- pages/[country]/index.tsx
+++ pages/[country]/index.tsx
@@ -33,7 +33,7 @@ export default function Home() {
   return (
     <>
       {/* <!-- Google tag (gtag.js) --> */}
-      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-7E286CBN97">
+      <Script id="gtm">
         {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
