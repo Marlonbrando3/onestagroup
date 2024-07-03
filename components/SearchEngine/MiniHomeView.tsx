8d027e8fb2c53d2b0a900d5b20d921c7349a191a
„PersonalTaxi” <marek.marszalek@onesta.com.pl>
2024-07-03 16:04:18 +0200

    first commit

--- components/SearchEngine/MiniHomeView.tsx
+++ components/SearchEngine/MiniHomeView.tsx
@@ -6,7 +6,7 @@ import OurJobTXT from "@/data/OurJob.json";
 import { IoIosArrowBack } from "react-icons/io";
 import { IoIosArrowForward } from "react-icons/io";
 
-export default function HowWeWork() {
+export default function MiniHomeView() {
   return (
     <div className='lg:w-full w-[90vw] rounded-b-[50px] lg:h-[200px] h-[670px] bg-cover bg-center bg-[url("/palmyBGform.jpeg")] mx-auto'></div>
   );
