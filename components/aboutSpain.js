90a56020e78d7cfa752450d03c420aeeb5ba38a9
„PersonalTaxi” <marek.marszalek@onesta.com.pl>
2024-07-04 10:00:32 +0200

    first commit

--- components/aboutSpain.js
+++ components/aboutSpain.js
@@ -13,38 +13,39 @@ export default function AboutSpain() {
           <Image src="/nieruchomosci-w-hiszpanii.jpeg" alt="nieruchomosci-w-hiszpanii" fill></Image>
         </div>
         <div className="ml-[20px]">
-          Inwestowanie w <strong>Nieruchomości w Hiszpanii</strong> stało się bardzo atrakcyjną
-          opcją. W ostatnich 2 lat ten region świata przeżył prawdziwy rozkwit. Pod od początku 2023
-          roku zakupu <strong>nieruchomości w Hiszpanii</strong> wzrół o kilkaset proces. Polacy
-          zostali zmobilizowani do rozważania tego kierunku między innymi przez konflikt na
-          wschodzie. Wiele osób przyspieszyło swoje plany o zakupie swojej własnej{" "}
-          <strong>nieruchomości w Hiszpanii</strong>, ponieważ jako najdalej wysunięte Państwa w
-          Europie dawało poczucie beziecznej inwestycji i faktycznie tak jest, nie zapowiada się
-          również aby w najbliższych latach co się zmieniło. Mimo tego, że rynek{" "}
-          <strong>nieruchomości w Hiszpanii</strong> zostal lekko wydrenowany i czas oczekiwania na
-          oddanie do użytku nowych inwestycji wzrósł z 3 do 17 miesięcy, to nadal nie daje
-          się zauważyć spadku zainteresownia tym regione, Ceny nieruchomości wzrosły o 5%, ponieważ
-          dalej szukając swojego drugiego <strong>domu w Hiszpanii</strong> nie liczymy tak bardzo
-          pieniędzy tylko marzenia. A one przecież ceny nie mają. <br></br>
+          Inwestowanie w <strong>Nieruchomości w Hiszpanii</strong> dla Polaków stało się bardzo
+          atrakcyjną możliwością. Od kilku lat ten region świata przeżywa prawdziwy rozkwit, a
+          wyłącznie od 2023 roku poziom zakupu <strong>nieruchomości w Hiszpanii</strong> wzrół o
+          kilkaset proces. Polacy zostali zmobilizowani do rozważania tego kierunku między innymi
+          przez konflikt na bliskim wschodzie. Wiele osób przyspieszyło swoje plany o zakupie swojej
+          własnej <strong>nieruchomości w Hiszpanii</strong>, ponieważ właśnie Hiszpania jako
+          najdalej wysunięte Państwo w Europie dawało poczucie bezpiecznej inwestycji i faktycznie
+          tak jest. Nie zapowiada się również aby w najbliższych latach co się zmieniło. Mimo tego,
+          że rynek <strong>nieruchomości w Hiszpanii</strong> wskutek trendu został lekko
+          wydrenowany i czas oczekiwania na oddanie do użytku nowych inwestycji wzrósł z 3 do 12
+          miesięcy, to nadal nie daje się zauważyć spadku zainteresownia zakupem, wręcz przeciwnie.
+          Ceny nieruchomości wzrosły o kolejne 5%, a to dlatego, że dla wielu lub dla zdecydowanej
+          większości kupujących zakup nieruchomości w Hiszpanii to prócz dobrej inwestycji również
+          często spełnienie ważnego marzenia, a te nie mają ceny. <br></br>
           <br></br>
-          Ma to swoje negatywne konswekcje bo wszędzie tam gdzie rynek kwitnie pojawią się
-          nadużycia. Od koncówki 2022 roku zauważamy duży wzrost agencji, które powstały aby
-          sprzedawać <strong>nieruchomości w Hiszpanii</strong> co powoduje, że jakość obsługi oraz
-          bezpieczeństwo w sprzedaży spadło proporcjonalnie ofert. Dostrzegliśmy to chcemy
-          zaznaczyć, że grupa <strong>Onesta</strong> (tłum. Uczciwość) dalej działa i utrzymuje
-          usługi o swoje wartości na których powstała: orientacja na klinta i wystepowanie w Jego
-          imieniu, uczciwość, elastyczność, relacyjność, wiedza. Po wielu procesach sprzedaży
-          nieruchomości w Hiszpanii, które przeprowadziliśmy lub przy których byliśmy obecni, wiemy
-          czego chcą (i nie chcą) nasi przyszli klienci.<br></br>
+          Od koncówki 2023 roku zauważamy również duży wzrost agencji, które powstały aby sprzedawać{" "}
+          <strong>nieruchomości w Hiszpanii</strong> co powoduje, że jakość obsługi oraz
+          bezpieczeństwo w sprzedaży spadło odwrotnie proporcjonalnie do wzrostu liczby ofert.
+          Dostrzegliśmy to i chcemy zaznaczyć, że grupa <strong>Onesta</strong> powstała z myślą i
+          zamiarem orientacji na klienta i wystepowanie w Jego imieniu w procesie zakupu/sprzedaży.
+          Dzieleniu się z wiedzą, byciem uczciwą, elastyczną, organizacją. Po wielu procesach
+          sprzedaży nieruchomości w Hiszpanii, które przeprowadziliśmy lub przy których byliśmy
+          obecni, wiemy czego chcą (i nie chcą) nasi klienci.<br></br>
           <br></br>
-          Możesz przeczytać jak zacząć (klik) poszukiwania swojej pierwszej{" "}
+          Zapraszamy do rozpoczęcia przygody od przeczytania o tym jak zacząć (klik) poszukiwania
+          swojej pierwszej{" "}
           <Link href="/blog/jak-kupic-nieruchomosc-w-hiszpanii" className="underline font-bold">
             Nieruchomości w Hiszpanii
           </Link>
           &nbsp; lub &nbsp;
-          <strong className="md:text-[26px] leading-8 text-green-600">
-            jeśli już znasz kryteria zgłosić się do nas bezpłatną konsultancje, poprzez formularz
-            niżej.
+          <strong className="font-bold">
+            jeśli wolisz porozmawaić zgłoś się do nas na bezpłatną konsultancje, poprzez formularz
+            niżej lub w menu strony.
           </strong>
         </div>
       </div>
