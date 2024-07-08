f8134b94a76297b1f3f6857217b802b913b5c900
„PersonalTaxi” <marek.marszalek@onesta.com.pl>
2024-07-08 16:39:50 +0200

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,15 +21,14 @@ export default function FirstView({ cookiesWindow }: AppProps) {
     <>
       {/* <!-- Hotjar Tracking Code for https://onesta.com.pl --> */}
       <Script id="hotjar">
-        {`
-                    (function(h,o,t,j,a,r){
-                        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                        h._hjSettings={hjid:3555670,hjsv:6};
-                        a=o.getElementsByTagName('head')[0];
-                        r=o.createElement('script');r.async=1;
-                        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                        a.appendChild(r);
-                    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        {`(function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:3555670,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=')
                     `}
       </Script>
       <Script id="gtm">
