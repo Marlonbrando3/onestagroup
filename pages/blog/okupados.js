31002c960abb86b76ac586d8f95769713a4fc707
„PersonalTaxi” <marek.marszalek@onesta.com.pl>
2024-07-09 09:00:36 +0200

    first commit

--- pages/blog/okupados.js
+++ pages/blog/okupados.js
@@ -26,7 +26,13 @@ export default function Okupados() {
   return (
     <>
       <Newsletter />
-      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-7E286CBN97"></Script>
+      <Script id="gtm">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-KL7WKBWL');`}
+      </Script>
       <Script id="google-analitycs">
         {`
         window.dataLayer = window.dataLayer || [];
