575cfa1193bade8fbf0985980741c96e936a807f
Marek <marek@MacBook-Air-Marek.local>
2024-07-31 10:46:57 +0200

    first commit

--- components/aboutOnestaMainPage.js
+++ components/aboutOnestaMainPage.js
@@ -8,11 +8,11 @@ export default function AboutOnestaMainPage() {
           <Image src="/bg_about.jpeg" alt="nieruchomosci-w-hiszpanii" fill></Image>
         </div>
         <div className="ml-[20px] text-[18px] md:mt-[20px] mt-auto">
-          <h1 className="w-[60%] text-right text-[34px] py-[10px] leading-[35px] mx-auto mb-[30px] ">
+          <h1 className="w-[80%] text-right text-[34px] py-[10px] leading-[35px] mx-auto mb-[30px] ">
             Multibroker <span className="font-bold">Onesta Group</span>
             <br></br>inny niż wszystkie
           </h1>
-          <p className="pl-[80px]">
+          <p className="md:pl-[80px]">
             <b>Dla Ciebie oznacza to równowagę w procesie poszukiwań</b> poprzez - współpracę z
             zespołem z 15 letnim doświadczeniem w sprzedaży i budowaniu relacji z klientami. Z
             organizacją, która słucha swoich rozmówców i wychodzi na przeciw Ich potrzebom.
