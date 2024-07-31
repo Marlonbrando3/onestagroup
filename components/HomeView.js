6ec0924c9a1c90352adbbdfe5ecb1e177bac5cd1
Marek <marek@MacBook-Air-Marek.local>
2024-07-31 10:37:26 +0200

    first commit

--- components/HomeView.js
+++ components/HomeView.js
@@ -12,7 +12,7 @@ function HomeView() {
       <iframe
         width="100%"
         height="845"
-        src="https://www.youtube.com/embed/YdGdmLXjaK4?si=k2HZL3B6xM_j9fVq&autoplay=1&muted=1&showinfo=0&controls=0&vq=hd1080&loop=1&playlist=YdGdmLXjaK4"
+        src="https://www.youtube.com/embed/YdGdmLXjaK4?si=k2HZL3B6xM_j9fVq&autoplay=1&mute=1&showinfo=0&controls=0&vq=hd1080&loop=1&playlist=YdGdmLXjaK4"
         title="YouTube video player"
         frameborder="0"
         autoplay
