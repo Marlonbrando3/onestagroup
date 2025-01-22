d10cc439d1cc5ca438b94f3ac863cefdbb0d2924
Marek <marek@MacBook-Air-Marek.local>
2025-01-22 17:42:12 +0100

    first commit

--- components/HomeView.js
+++ components/HomeView.js
@@ -3,8 +3,8 @@ import { IoIosArrowDropdownCircle } from "react-icons/io";
 
 function HomeView() {
   return (
-    <div className="relative lg:h-[600px] md:w-[1500px] h-screen disable overflow-hidden mx-auto">
-      <div className="bottom-0 absolute w-screen h-[250px] bg-gradient-to-b from-transparent from-20% via-white via-70% to-white to-100% overflow-hidden"></div>
+    <div className="relative lg:h-[700px] md:w-[1500px] h-screen disable overflow-hidden mx-auto">
+      <div className="bottom-0 absolute w-screen h-[150px] bg-gradient-to-b from-transparent from-20% via-white via-70% to-white to-100% overflow-hidden"></div>
       <div className='md:h-full h-[100vh] lg:w-full w-screen mx-auto duration-500 sm:-mt-10 bg-cover bg-top bg-[url("/bg_calp_c.jpg")] md:hidden visibile'>
         <div className="absolute transition-transform bg-white z-20 w-full h-full -top-full"></div>
       </div>
@@ -12,14 +12,14 @@ function HomeView() {
       <iframe
         width="100%"
         height="845"
-        src="https://www.youtube.com/embed/QSKrrqmxpaw?si=g_hflB_vSSZLszs5&autoplay=1&mute=1&showinfo=0&controls=0&vq=hd1080&loop=1"
+        src="https://www.youtube.com/embed/QSKrrqmxpaw?si=g_hflB_vSSZLszs5&autoplay=1&mute=1&showinfo=0&controls=0&vq=hd1080&loop=1&playlist=QSKrrqmxpaw"
         title="YouTube video player"
         frameborder="0"
         autoplay
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerpolicy="strict-origin-when-cross-origin"
         allowfullscreen
-        className="-mt-[170px] hidden md:block"
+        className="-mt-[100px] hidden md:block"
       ></iframe>
       {/* </div> */}
       {/* <div className="w-[70px] h-[70px] z-50 absolute bottom-32 left-0 right-0 mx-auto md:hidden visible">
