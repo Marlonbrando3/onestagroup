1983c5decfb248812ed13308f413dda5bfbaa254
Marek <marek@MacBook-Air-Marek.local>
2025-05-12 21:30:51 +0200

    first commit

--- components/SearchEngine/MiniHomeView.tsx
+++ components/SearchEngine/MiniHomeView.tsx
@@ -2,6 +2,9 @@ import React, { useRef, useState } from "react";
 
 export default function MiniHomeView() {
   return (
-    <div className='lg:w-full w-full lg:h-[100px] h-[140px] bg-cover bg-[center_430px]  mx-auto '></div>
+    <div className="lg:w-full w-full lg:h-[200px] h-[140px]  mx-auto">
+      <div className=" bg-white w-full h-[60px]"></div>
+      <div className=" bg-white w-full h-full bg-[url('/bg_2.png')] bg-cover md:bg-[center_420px] bg-[center_320px]"></div>
+    </div>
   );
 }
