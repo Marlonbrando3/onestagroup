b48ad9acdc5b2c2073b869caaca7a5d77a160553
Marco <marco@Marcos-MacBook-Pro.local>
2025-12-23 20:37:52 +0100

    first commit

--- components/MiddlePageOne.tsx
+++ components/MiddlePageOne.tsx
@@ -7,16 +7,16 @@ import Image from "next/image";
 export default function MiddlePageOne() {
   return (
     <div
-      className={`${MontserratSans.className} w-[93vw] rounded-[30px] md:h-[340px] h-[300px] my-[100px] relative `}
+      className={`${MontserratSans.className} w-[93vw] rounded-[30px] md:h-[340px] h-[300px] my-[100px] mx-auto relative `}
     >
-      <p className="absolute top-0 font-[600] md:text-[70px] text-[40px] text-[#275278] w-full md:w-auto z-30 md:right-[60px] text-center right-[0%] md:leading-[70px] leading-[40px] md:top-0 bottom-0 my-auto grid place-items-center top-[50%] md:text-right bg-gradient-to-b from-white/[0.0] from-0% via-white via-25% to-100% to-white md:bg-gradient-to-b md:from-white/[0.0] md:from-0% md:via-transparent md:via-25% md:to-100% md:to-transparent md:pt-[80px] md:pb-[80px]">
+      <p className="absolute top-0 font-[600] md:text-[70px] text-[40px] text-[#275278] w-full md:w-auto z-30 md:right-[60px] text-center right-[0%] md:leading-[70px] leading-[40px] md:top-0 bottom-0 my-auto grid place-items-center top-[50%] md:text-right bg-gradient-to-b from-white/[0.0] from-0% via-white via-25% to-100% to-white md:bg-gradient-to-b md:from-white/[0.0] md:from-0% md:via-transparent md:via-25% md:to-100% md:to-transparent pt-[80px] pb-[80px]">
         Twoje marzenia, <br></br>nasze cele.
       </p>
       <Image
         src="/MiddleOne.png"
         fill
         // objectFit="cover"
-        className="object-cover md:object-[60%_65%] object-[0%_100%] rounded-[30px] right-0 pb-[50px] md:pb-0"
+        className="object-cover md:object-[60%_65%] object-[0%_300%] rounded-[30px] right-0 pb-[50px] md:pb-0"
         alt="spain"
       ></Image>
     </div>
--- components/MiddlePageOneSecond.tsx
+++ components/MiddlePageOneSecond.tsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 export default function MiddlePageOneSecond() {
   return (
     <div
-      className={`${MontserratSans.className} w-[93vw] rounded-[30px] h-[340px] my-[100px] relative overflow-hidden bg-white`}
+      className={`${MontserratSans.className} w-[93vw] rounded-[30px] h-[340px] my-[100px] mx-auto relative overflow-hidden bg-white`}
     >
       <p className="absolute lg:top-0 font-[600] lg:text-[70px] md:text-[50px] text-[40px] text-[#275278] z-20 md:right-[0px] w-full lg:w-auto lg:leading-[70px] md:leading-[50px] leading-[40px] -bottom-[1px] my-auto grid place-items-center lg:bg-gradient-to-r md:from-white/[0.0] md:from-0%  md:via-white md:via-25% md:to-100% md:to-white bg-gradient-to-b from-white/[0.0] from-0% via-white via-25% to-100% to-white lg:pl-[200px] pt-[80px] pb-[40px] ">
         Wiele potrzeb, <br></br>jeden model.
