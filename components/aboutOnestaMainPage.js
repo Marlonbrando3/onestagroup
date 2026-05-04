ee0eedc8287b3612c3d900de46b000b35ddc3e96
Marco <marco@Marcos-MacBook-Pro.local>
2026-05-04 12:33:04 +0200

    first commit

--- components/aboutOnestaMainPage.js
+++ components/aboutOnestaMainPage.js
@@ -7,7 +7,7 @@ export default function AboutOnestaMainPage() {
       className={`${Red_Hat_DisplayFont.className} w-screen h-auto py-[180px]`}
     >
       <div className="lg:w-[90vw] w-[90vw] mx-auto lg:h-[600px] flex justify-end relative">
-        <div className="absolute lg:h-full h-[750px] w-[90%] bg-[url('/bg_second.png')] bg-cover rounded-[4px] bg-fixed left-0"></div>
+        <div className="absolute lg:h-full h-[750px] w-[90%] bg-[url('/bg_second.webp')] bg-cover rounded-[4px] bg-fixed left-0"></div>
         <div className=" w-[96%] h-[94%] top-0 bottom-0 left-0 right-0 m-auto border-[1.6px] border-orange-500 rounded-[4px] absolute "></div>
         <div className="ml-[20px] w-[800px] md:h-[600px] h-[850px] text-[18px] md:mt-[80px] mt-auto bg-white rounded-[4px] shadow-[0_0px_10px_0_rgba(0,0,0,0.3)] relative">
           <h1 className="w-[80%] text-left text-[34px] py-[10px] leading-[35px] mx-auto mb-[30px]">
